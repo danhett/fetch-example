a36bbbddb70321e3e2d93043e86a5a2df3cc8a0d
Dan Hett <hellodanhett@gmail.com>
2021-04-19 14:17:34 +0100

    Test update

--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 /* insert interactivity here! */
 
-window.addEventListener("load", () => {
-  console.log("Up and running.")
+window.addEventListener("load", () => {  
+  let loc = "Manchester,uk";
+  let key = "XXXXXX"
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${loc}&appid=${key}`;
+  
+
+  fetch(url)
+  .then(response => response.json())
+  .then(data => processData(data));
 })
+
+function processData(data) {
+  // get the part we need, which is today's description text
+  let responseText = data.weather[0].description.toLowerCase();
+
+  // check to see if the text is present.
+  if(responseText.indexOf("rain") == -1) {
+    console.log("It is not raining.");
+  }
+  else {
+    console.log("It is raining.");
+  }
+
+  // OTHER WAYS TO DO THIS:
+  // - check weather code range (see docs)
+  // - check precipitation level etc
+  // but you get the idea. :)
+}
